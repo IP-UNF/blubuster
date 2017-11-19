3c01125596b03efb7191ca80fef4d13e47b1f0a2
Jerrad Monagan <jerradmonagan@gmail.com>
2017-11-18 21:10:05 -0500

    added the 900x350 sale adds, placeholders will look for better ones lare

--- body.php
+++ body.php
@@ -55,13 +55,13 @@
         </ol>
         <div class="carousel-inner" role="listbox">
           <div class="carousel-item active">
-            <img class="d-block img-fluid" src="" alt="First slide">
+            <img class="d-block img-fluid"src="./images/magnum.jpg" src="" alt="Magnum PI">
           </div>
           <div class="carousel-item">
-            <img class="d-block img-fluid" src="http://placehold.it/900x350" alt="Second slide">
+            <img class="d-block img-fluid" src="./images/btf.jpg" alt="Back to the Futre">
           </div>
           <div class="carousel-item">
-            <img class="d-block img-fluid" src="http://placehold.it/900x350" alt="Third slide">
+            <img class="d-block img-fluid" src="./images/nfl.jpg" alt="NFL">
           </div>
         </div>
         <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">

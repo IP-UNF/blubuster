f166b4ea8dcf8fb838abc242431c9b5bada04775
Jerrad Monagan <jerradmonagan@gmail.com>
2017-12-05 11:31:40 -0500

    added about info

--- about.php
+++ about.php
@@ -2,4 +2,12 @@
 <?php include './sharedlayout/header.php';?>
 <?php session_start();?>
 <?php include './sharedlayout/body.php';?>
+<h1> <strong>Blubuster Video by Blu-Devils</strong><h1><br>
+    <p style="font-size:20px">We Started out as University Project in 2017 for an Internet Programming class.
+      As Team of four we worked hard to get out our first prototype to achieve the best grade we could.</p>
+    <p style="font-size:20px">By the end of that semester we had offers for 7 different Venture Captalist firms across the United
+      States. We started with an initial funding of 40 Million dollars, which gave us the runway we needed
+      to create the awesome site you see today</p>
+      <a  href="mailto:blubustervideo@gmail.com">Contatct US</a>
+
 <?php include './sharedlayout/footer.php';?>
